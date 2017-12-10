d81f1401e140b8d4adc7b41669da5048bc3cc1f1
mdrakib19 <mdrakib19@gmail.com>
2017-12-11 02:07:52 +0600

    command

---
+++ welcome.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+void welcome()
+{
+    printf("\n\t\t Welcome to Teacher's Review of BUBT CSE Department\n");
+    printf("\t\t --------------------------------------------------\n\n");
+}
