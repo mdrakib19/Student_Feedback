6de56edfe33937392f2c22bd0de9cc1aeedae3d3
mdrakib19 <mdrakib19@gmail.com>
2017-12-11 02:19:46 +0600

    command

---
+++ logout.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+int login_status;
+int reg_status;
+char logged_in_name[20];
+char logged_in_id[20];
+
+void logout()
+{
+    int check;
+
+    printf("\n1.Yes\n2.No\n\n>_");
+    scanf("%d", &check);
+
+    while(check != 1 && check != 2)
+    {
+        printf("\n\nWrong input!");
+        printf("\n1.Yes\n2.No\n");
+
+    }
+
+    if(check == 2)
+    {
+        login_status = 0;
+        reg_status = 0;
+        strcpy(logged_in_name, "\0");
+        strcpy(logged_in_id, "\0");
+    }
+}
+
